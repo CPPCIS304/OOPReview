d4f11cfd38fc9c592ea7dd3cf2f58af4708013db
Stephen Briles <stephenbriles@gmail.com>
2016-10-02 21:58:25 -0700

    Final Updates

---
+++ LoanMonthlyPayment/test/LoanTest/LoanTest.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LoanTest;
+
+import Loan.LoanMonthlyPayment;
+
+/**
+ *
+ * @author Roberto
+ */
+public class LoanTest {
+    public static void main(String[] args) {
+        System.out.println("Test Case 1");
+        LoanMonthlyPayment loan = new LoanMonthlyPayment(350000, 0, 10, 15);
+        displayInputs(loan);
+        System.out.println("Montly payment: " + loan.calcMonthlyPayment());
+
+        System.out.println("\nTest Case 2");
+        loan.setDownPayment(50000);
+        displayInputs(loan);
+        System.out.println("Montly payment: " + loan.calcMonthlyPayment());
+
+        System.out.println("\nTest Case 3");
+        loan.setDownPayment(0);
+        loan.setAPR(5);
+        displayInputs(loan);
+        System.out.println("Montly payment: " + loan.calcMonthlyPayment());
+
+        System.out.println("\nTest Case 4");
+        loan.setDownPayment(50000);
+        displayInputs(loan);
+        System.out.println("Montly payment: " + loan.calcMonthlyPayment());
+
+    }
+
+    private static void displayInputs(LoanMonthlyPayment loan) {
+        System.out.println("Purchase Amount: " + loan.getPurchaseAmount());
+        System.out.println("Down Payment: " + loan.getDownPayment());
+        System.out.println("APR (%): " + loan.getAPR());
+        System.out.println("Duration (years): " + loan.getYears());
+    }
+}
+
+
