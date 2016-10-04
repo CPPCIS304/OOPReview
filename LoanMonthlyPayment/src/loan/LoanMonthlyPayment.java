8777d09acb6a25d94b44324b770e9bf9a2fab23f
Stephen Briles <stephenbriles@gmail.com>
2016-10-03 17:08:12 -0700

    Minor Changes and Doc file upload

--- LoanMonthlyPayment/src/loan/LoanMonthlyPayment.java
+++ LoanMonthlyPayment/src/loan/LoanMonthlyPayment.java
@@ -1,22 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package Loan;
+
+package loan;
+
 /**
- * The class constructor creates a LoanMonthlyPayment object configured to
- * calculate a loan payment. Two constructors are required, one that does not
- * accept parameters, and one that does. Where no parameters are used, the “set”
- * methods must be used to store loan parameters within the resulting object.
- * Where parameters are used, the loan parameters are provided to the object
- * through the constructor.
- * 
- * @author Sowathana Dennie Hem
+The LoanMonthlyPayment class calculates the monthly payment needed to pay off
+ * a loan. The class API is based on a purchase amount and a down payment.
+ * Internally, the class calculates the amount to be financed by the loan as the
+ * purchase amount less the down payment.
+ *
+ * @author 127.0.0.1
  */
-
-
 public class LoanMonthlyPayment {
+
+
+   
     private double purchaseAmount;
     private double downPayment;
     private double apr;
@@ -37,18 +33,25 @@ public class LoanMonthlyPayment {
         loanAmount = 0;
         periods = 0;
         pr = 0;
-        
-        
     }
- /**
+    
+    /**
+     * The class constructor creates a LoanMonthlyPayment object configured to
+     * calculate a loan payment. Two constructors are required, one that does
+     * not accept parameters, and one that does. Where no parameters are used,
+     * the “set” methods must be used to store loan parameters within the
+     * resulting object. Where parameters are used, the loan parameters are
+     * provided to the object through the constructor.
+     *
+     * @param purchaseAmount - the amount of the purchase to be made by the
+     * borrower
+     * @param downPayment – the down payment to be made by the borrower against
+     * the purchase amount
+     * @param apr – the annual percentage rate (APR) to be applied to the loan.
+     * @param years - the duration of the loan in years
+     */
+     
 
- * 
- * @param purchaseAmount - the amount of the purchase to be made by the borrower
- * @param downPayment – the down payment to be made by the borrower against the purchase amount
- * @param apr – the annual percentage rate (APR) to be applied to the loan.
- * @param years - the duration of the loan in years
- *
- */
      public LoanMonthlyPayment(double purchaseAmount,
             double downPayment,
             double apr, double years) {
@@ -134,3 +137,6 @@ public class LoanMonthlyPayment {
     }
             
 }
+        
+    
+    
--- LoanMonthlyPayment/src/loanTest/LoanTest.java
+++ LoanMonthlyPayment/src/loanTest/LoanTest.java
@@ -3,9 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package LoanTest;
+package loanTest;
 
-import Loan.LoanMonthlyPayment;
+import loan.LoanMonthlyPayment;
 
 /**
  *
